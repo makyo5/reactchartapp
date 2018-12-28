ce6a1ca5151f8395c9b76885fb59192cce3f1a75
syl-work-pc <2581028207@qq.com>
2018-12-28 20:20:10 +0800

    add viser-react, echarts-for-react

--- src/component/xxkworkstatic.js
+++ src/component/xxkworkstatic.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 import { Button } from 'antd';
+import * as av from '@antv/g2';
+
+const data = [
+  { genre: 'Sports', sold: 274 },
+  { genre: 'Strategy', sold: 115},
+];
+
+const Chart = av.Chart;
 
 class XxkWorkStatic extends Component {
   render(){
     return (
       <div>
         xxkworks Charts
+        <Chart />
       </div>
     )
   }
