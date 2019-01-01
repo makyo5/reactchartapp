94e00deb358005c04058ec48478021fe3660c4b5
Le <2581028207@qq.com>
2019-01-01 22:48:17 +0800

    mock

--- src/layout/dashboard.js
+++ src/layout/dashboard.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import React, { Component } from "react";
+import { Row, Col } from "antd";
 
-import {XxkWorkStatic,ECWorkStatics,ECWorkDayStatics} from '../component/xxkworkstatic';
-
+import {
+  XxkWorkStatic,
+  ECWorkStatics,
+  ECWorkDayStatics
+} from "../component/xxkworkstatic";
 
 class Dashboard extends Component {
-    render () {
-        return (
-            <div>
-                <Row>
-                    <Col span={6}>
-                        <XxkWorkStatic/>
-                    </Col>
-                    <Col span={6}>
-                        <ECWorkStatics />
-                    </Col>
-                    <Col span={6}>
-                        <ECWorkDayStatics/>
-                    </Col>
-                    <Col span={6}>
-                        <XxkWorkStatic/>
-                    </Col>
-                </Row>
-                        
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col span={6}>
+            <XxkWorkStatic />
+          </Col>
+          <Col span={6}>
+            <ECWorkStatics />
+          </Col>
+          <Col span={6}>
+            <ECWorkDayStatics />
+          </Col>
+          <Col span={6}>
+            <XxkWorkStatic />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 export default Dashboard;
